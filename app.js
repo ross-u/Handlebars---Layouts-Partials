a00a6aedf32706a46aa4532a025eee67e9c86ef8
ross-u <cirkovic.uros.srb@gmail.com>
2019-04-24 03:13:19 +0200

    Update file structure

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
+const players = require('./playersData');
+
 
 // SET THE TEMPLATE ENGINE
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
+
+
 // ROUTES
 app.get('/', (req, res, next) => {
   res.render('index');
-});
-
-app.get('/players', (req, res, next) => {
-  res.render('players');
 });
 
 app.get('/teams', (req, res, next) => {
   res.render('teams');
 });
 
+app.get('/players', (req, res, next) => {
+  res.render('players', data );
+});
+
 // START THE SERVER
-app.listen(PORT, () => console.log('Server listening on a PORT ${PORT}'));
+app.listen(PORT, () => console.log(`Server listening on a PORT ${PORT}`));
